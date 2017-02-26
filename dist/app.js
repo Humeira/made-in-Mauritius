d9d44b1cbd73c0fcc9865e83b8ffa8dea25ac2f9
Humeira Diljore <hello@echdee.me>
2017-02-26 14:06:47 +0400

    🎉  compiled new list

--- dist/app.js
+++ dist/app.js
@@ -32069,6 +32069,12 @@ module.exports = Vue$2;
         repo_link: 'https://github.com/Humeira/made-in-Mauritius',
         description: 'A list of cool open-source projects made in Mauritius',
         language_tag: 'Vue'
+    }, {
+        github_username: 'Nayar',
+        repo_name: 'KDE-for-Mauritius',
+        repo_link: 'https://github.com/Nayar/KDE-for-Mauritius',
+        description: 'Adapting KDE in the Mauritian Context',
+        language_tag: 'Bash'
     }]
 };
 
