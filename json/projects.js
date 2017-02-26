1249ee9ed061364d8dd9cb0f22e6eaf654fa1d5e
Nayar Joolfoo <Nayar@users.noreply.github.com>
2017-02-26 13:52:23 +0400

    Add KDE-for-Mauritius

--- json/projects.js
+++ json/projects.js
@@ -6,6 +6,13 @@ export default {
             repo_link: 'https://github.com/Humeira/made-in-Mauritius',
             description: 'A list of cool open-source projects made in Mauritius',
             language_tag: 'Vue'
+        },
+        {
+            github_username: 'Nayar',
+            repo_name: 'KDE-for-Mauritius',
+            repo_link: 'https://github.com/Nayar/KDE-for-Mauritius',
+            description: 'Adapting KDE in the Mauritian Context',
+            language_tag: 'Bash'
         }
     ]
 }
