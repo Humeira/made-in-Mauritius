208eaac327c4b74c3ac8e99a760a5d9fd9f7ae00
Humeira Diljore <hello@echdee.me>
2017-04-04 19:58:50 +0400

    Added @percymamedy projects 🎉

--- dist/app.js
+++ dist/app.js
@@ -32081,6 +32081,36 @@ module.exports = Vue$2;
         repo_link: 'https://github.com/mscraftsman/devcon2017',
         description: 'Website of Developers Conference 2017 in Mauritius ',
         language_tag: 'CSS'
+    }, {
+        github_username: 'percymamedy',
+        repo_name: 'Laravel Dev Booter',
+        repo_link: 'https://github.com/percymamedy/laravel-dev-booter',
+        description: 'Boost your Laravel app by registering Prod services only on Prod.',
+        language_tag: 'PHP'
+    }, {
+        github_username: 'percymamedy',
+        repo_name: 'PHP IBM Watson API Bridge',
+        repo_link: 'https://github.com/findbrok/php-watson-api-bridge',
+        description: 'A PHP and Laravel wrapper for IBM Watson API.',
+        language_tag: 'PHP'
+    }, {
+        github_username: 'percymamedy',
+        repo_name: 'Laravel 5 Personality Insights',
+        repo_link: 'https://github.com/findbrok/laravel-personality-insights',
+        description: 'Using IBM Watson Personality Insights Service with Laravel 5.',
+        language_tag: 'PHP'
+    }, {
+        github_username: 'percymamedy',
+        repo_name: 'Mango Repo',
+        repo_link: 'https://github.com/LaraChimp/mango-repo',
+        description: 'Repository Pattern for Laravel 5 made simple.',
+        language_tag: 'PHP'
+    }, {
+        github_username: 'percymamedy',
+        repo_name: 'Crafter',
+        repo_link: 'https://github.com/percymamedy/crafter',
+        description: 'Install your favourite PHP frameworks using only one installer.',
+        language_tag: 'PHP'
     }]
 };
 
